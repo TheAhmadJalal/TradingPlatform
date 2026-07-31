3947493a2018d8d96915daea5e3c79ff3a519e25
TheAhmadJalal <ahmadjalal110101@gmail.com>
2026-07-31 19:55:04 +0500

    api url changed

--- Project/CRM/crm-frontend/js/config.js
+++ Project/CRM/crm-frontend/js/config.js
@@ -5,7 +5,7 @@
 // var API_URL = "http://localhost:5001";
 // var API_URL = "http://45.225.135.194:5001";
 // var API_URL = "http://cedarcapitalgp.com:5001";
-var API_URL = "";
+var API_URL = "/crm-api";
 
 // also expose on window so inline <script> blocks can read it
 window.API_URL = API_URL;
