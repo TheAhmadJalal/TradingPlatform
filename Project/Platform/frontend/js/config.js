0b08273d90884b9af5e3b4c090825f70382653f5
TheAhmadJalal <ahmadjalal110101@gmail.com>
2026-07-31 19:20:01 +0500

    api url changed

--- Project/Platform/frontend/js/config.js
+++ Project/Platform/frontend/js/config.js
@@ -4,7 +4,7 @@
 // ---------------------------------------------------------------
 // var API_URL = "http://45.225.135.194:5000";
 // var API_URL = "http://cedarcapitalgp.com:5000";
-const API_URL = "/api";
+const API_URL = "";
 
 // also expose on window so inline <script> blocks can read it
 window.API_URL = API_URL;
